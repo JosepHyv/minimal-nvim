26ef58ae95680a5c10a5927c6d42321fcce4bbdd
josephy <josephynimoto@gmail.com>
2026-02-24 23:40:59 -0600

    fix: adding keymaps for definitions, and enabling treesittier features

--- lua/plugins/mason-lsp.lua
+++ lua/plugins/mason-lsp.lua
@@ -29,16 +29,23 @@ return {
 				"lua_ls",
 				"cssls",
 				"pyright",
-				-- "tree-sitter-cli",
-				"stylua",
 			},
-			html = {},
-			ruff = {},
-			gopls = {},
-			clangd = {},
-			ts_ls = {},
-			tailwindcss = {},
-			pyright = {},
 		},
+		config = function(_, opts)
+			require("mason-lspconfig").setup(opts)
+		end,
+	},
+	{
+		"neovim/nvim-lspconfig",
+		dependencies = {
+			"mason-org/mason.nvim",
+		},
+		config = function()
+			local key = vim.keymap
+			key.set("n", "gd", vim.lsp.buf.definition, { desc = "Go to Definition" })
+			key.set("n", "gD", vim.lsp.buf.declaration, { desc = "Go to Declaration" })
+            key.set("n", "gr", vim.lsp.buf.references, {desc = "Go to references"})
+			key.set("n", "K", vim.lsp.buf.hover, { desc = "Hover Information" })
+		end,
 	},
 }
--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -29,4 +29,6 @@ return {
 	indent = {
 		enable = true,
 	},
+	highlight = { enable = true },
+	folds = { enable = true },
 }
