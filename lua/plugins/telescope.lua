0a7e674c0dcdb994ad8b8e9c279502937dc8437b
josephy <josephynimoto@gmail.com>
2026-02-25 00:24:32 -0600

    add keymap for lsp document symbols

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -4,6 +4,7 @@ return {
      keys = {
         {"<leader> ", "<cmd>Telescope find_files<cr>", desc = "Find Files"},
         {"<leader>/", "<cmd>Telescope live_grep<cr>", desc = "Grep String in files with live preview"},
-        {"<leader>,", "<cmd>Telescope buffers<cr>", desc = "Grep String in files with live preview"}
+        {"<leader>,", "<cmd>Telescope buffers<cr>", desc = "Grep String in files with live preview"},
+        {"<leader>s", "<cmd>Telescope lsp_document_symbols<cr>", desc = "Show document symbols"},
      }
 }
