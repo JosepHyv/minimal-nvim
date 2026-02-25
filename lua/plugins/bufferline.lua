9776b6a1e241bd99fd2679b2eeb4424faf54dc4b
josephy <josephynimoto@gmail.com>
2026-02-25 00:25:14 -0600

    new styles and colors

--- lua/plugins/bufferline.lua
+++ lua/plugins/bufferline.lua
@@ -1,22 +1,34 @@
 return {
-    'akinsho/bufferline.nvim', 
-    version = "*", 
-    dependencies = 'nvim-tree/nvim-web-devicons', 
-    event = "VeryLazy", 
-    opts = {
-        options = {
-            mode = "buffers", 
-            show_buffer_close_icons = true, 
-            always_show_bufferline = false,
-            -- separator_style = "thick",
-            indicator = {
-                icon = "| ",
-                style = "underline"
-            }, 
-            diagnostics_indicator = function(count, level, diagnostics_dict, context)
-                return "("..count..")"
-            end,
-        }
-    }
+	"akinsho/bufferline.nvim",
+	version = "*",
+	dependencies = "nvim-tree/nvim-web-devicons",
+	event = "VeryLazy",
+	opts = {
+		options = {
+			padding = 1,
+			mode = "buffers",
+			show_buffer_close_icons = true,
+			always_show_bufferline = false,
+			separator_style = "slant",
+			-- indicator = {
+			-- 	icon = "| ",
+			-- 	style = "underline",
+			-- },
+			diagnostics_indicator = function(count, level)
+				local icon = level:match("error") and " " or " "
+				return " " .. icon .. count
+			end,
+			offsets = {
+				{
+					filetype = "NvimTree",
+					text = "File Explorer",
+					text_align = "center",
+					separator = true,
+				}
+			},
+			-- diagnostics_indicator = function(count, level, diagnostics_dict, context)
+			-- 	return "(" .. count .. ")"
+			-- end,
+		},
+	},
 }
-
--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,7 +1,7 @@
 return {
 	"sainnhe/sonokai",
 	lazy = false,
-	priority = 1000,
+	-- priority = 1000,
 	config = function()
 		-- vim.g.sonokai_transparent_background = "2"
 		vim.g.sonokai_enable_italic = "1"
